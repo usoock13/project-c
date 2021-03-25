9f1a1e7a029f4c660deb39b79c242e47023e4aac
usoock13 <usoock13@gmail.com>
2021-03-25 22:36:45 +0900

    usw

---
+++ project-c/Assets/Script/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody playerRigidbody;
+    Animator playerAnimator;
+    public GameObject playerAvatar;
+
+    float moveSpeed = 10f;
+
+    void Start() {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = playerAvatar.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        float xx = Input.GetAxisRaw("Horizontal");
+        float yy = Input.GetAxisRaw("Vertical");
+
+        if(xx==0 && yy==0) {
+            playerAnimator.SetBool("Running", false);
+        }
+    }
+
+    public void Move(Vector3 direction) {
+        // Position
+        Vector3 addPosition = transform.TransformDirection(direction);
+        playerRigidbody.MovePosition(transform.position + (addPosition * Time.deltaTime * moveSpeed));
+        
+        // Rotate
+        Vector3 rotateTarget = transform.TransformDirection(direction);
+        playerAvatar.transform.rotation = Quaternion.Slerp(playerAvatar.transform.rotation, Quaternion.LookRotation(rotateTarget), Time.deltaTime * 10f);
+
+        // Animation
+        playerAnimator.SetBool("Running", true);
+    }
+}
---
+++ project-c/Assets/Script/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    Player playerScript;
+    void Awake() {
+        playerScript = GetComponent<Player>();
+    }
+    void Update()
+    {
+        MoveInput();
+    }
+    void MoveInput(){
+        float xx = Input.GetAxisRaw("Horizontal");
+        float yy = Input.GetAxisRaw("Vertical");
+
+        if(xx!=0 || yy!=0) {
+            Vector3 direction = new Vector3(xx, 0, yy).normalized;
+            playerScript.Move(direction);
+        }
+    }
+}
