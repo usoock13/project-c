f42e9ff08b3cb28e404cb328fcaba42e83fbdc15
usoock13 <usoock13@gmail.com>
2021-03-29 21:06:04 +0900

    force-push

---
+++ Assets/Script/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    Player playerScript;
+    void Awake() {
+        playerScript = GetComponent<Player>();
+    }
+    void FixedUpdate()
+    {
+        MoveInput();
+    }
+    void MoveInput(){
+        float xx = Input.GetAxisRaw("Horizontal");
+        float yy = Input.GetAxisRaw("Vertical");
+
+        if(xx!=0 || yy!=0) {
+            Vector3 direction = new Vector3(xx, 0, yy).normalized;
+            playerScript.Move(direction);
+        } else {
+            
+        }
+    }
+}
